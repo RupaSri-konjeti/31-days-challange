ac14fe380a7ba236f69c5bb3add7d842aac3e0c8
RupaSri-konjeti <konjetirupasri@gmail.com>
2026-06-20 20:35:54 +0530

    Move zero to last pof the array

---
+++ Day 13/MoveZero.java
@@ -0,0 +1,21 @@
+//move zero 
+import java.util.*;
+class Main{
+    public static void main(String args[])
+    {
+        int arr[]={0,20,30,0,4,0,4,3,2,0};
+        int j=0;
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]!=0)
+            {
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+                
+                j++;
+            }
+        }
+        System.out.println(Arrays.toString(arr));//it need to convert other wise gives the memory address
+    }
+}
