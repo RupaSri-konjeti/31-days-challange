72e2fb2345e7ab367f5ef00988d526d985ff4dda
RupaSri-konjeti <konjetirupasri@gmail.com>
2026-06-24 15:48:27 +0530

    Maximum Circular Subarray Sum solution

--- Day 15/Maximum Circular Subarray.java
+++ Day 15/Maximum Circular Subarray.java
@@ -1,8 +1,75 @@
+// Maximum Circular Subarray Sum - Optimal Solution (O(n))
+import java.util.Scanner;
+
+class Main {
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter number of elements:");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int total = 0;
+
+        int currentMax = 0;
+        int maxSum = arr[0];
+
+        int currentMin = 0;
+        int minSum = arr[0];
+
+        for (int i = 0; i < n; i++) {
+
+            total += arr[i];
+
+            // Maximum Subarray (Kadane)
+            currentMax = Math.max(arr[i], currentMax + arr[i]);
+            maxSum = Math.max(maxSum, currentMax);
+
+            // Minimum Subarray (Reverse Kadane)
+            currentMin = Math.min(arr[i], currentMin + arr[i]);
+            minSum = Math.min(minSum, currentMin);
+        }
+
+        int answer;
+
+        // Handle all negative elements
+        if (maxSum < 0) {
+            answer = maxSum;
+        } else {
+            int circularSum = total - minSum;
+            answer = Math.max(maxSum, circularSum);
+        }
+
+        System.out.println("Maximum Subarray Sum = " + maxSum);
+        System.out.println("Minimum Subarray Sum = " + minSum);
+        System.out.println("Maximum Circular Subarray Sum = " + answer);
+    }
+}
+
+
+
+
+
+/*
 //Maximum Circular Subarray Sum
 import java.util.Scanner;
 class Main{
     public static void main(String args[]){
-        int arr[]={10,20,30,40,50,-1,4,-3};
+        Scanner sc =new Scanner (System.in);
+       System.out.println("neku kavasinavi nokuko");
+       int n = sc.nextInt();
+       int arr[]=new int[n];
+       for(int i=0;i<n;i++)
+       {
+           arr[i]=sc.nextInt();
+       }
         int total=0;
         int currentMax=0;
         int Max=arr[0];
@@ -30,3 +97,4 @@ class Main{
     }
     
 }
+*/
